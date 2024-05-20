eae25f060eb65aae3a9272b0ac7c121ea7429ced
Charles Caleb <charlesofficial9@gmail.com>
2024-05-20 06:48:54 +0100

    login update

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("calebcharles343@gmail.com");
+  const [password, setPassword] = useState("11111111");
 
   const { login, isLoading } = useLogin();
 
